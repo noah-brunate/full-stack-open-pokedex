05701a1e3cfa9d98389ed7db81cffc9587de82b1
noah-brunate <noahbrunate9@gmail.com>
2025-02-05 11:11:01 +0300

    error eslint rect

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double", "single"],
+    quotes: ["error", "double"],
     semi: ["error", "never"],
     eqeqeq: "error",
     "no-trailing-spaces": "error",
